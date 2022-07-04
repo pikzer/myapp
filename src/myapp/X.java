e5452597438232e52bbdac3a14282699f05633a8
pikzer <60600988+pikzer@users.noreply.github.com>
2022-07-04 10:00:26 +0700

    make conflict2

--- src/myapp/X.java
+++ src/myapp/X.java
@@ -3,16 +3,8 @@ package myapp;
 public class X {
     public static void main(String[] args) {
         System.out.print("Operation XX");
-        System.out.print("Operation XXX");
-        public static void ee (){
-
-        }
-
-
-        // Git merge
-
-        //:wqt!
-
+    }
+    public static void ee (){
 
     }
 }
