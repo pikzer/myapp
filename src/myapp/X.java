65e82489f109e2c150e1b0952c124542530b29cc
pikzer <60600988+pikzer@users.noreply.github.com>
2022-07-04 09:59:35 +0700

    make conflict

--- src/myapp/X.java
+++ src/myapp/X.java
@@ -2,10 +2,12 @@ package myapp;
 
 public class X {
     public static void main(String[] args) {
-
         System.out.print("Operation XX");
-
         System.out.print("Operation XXX");
+        public static void ee (){
+
+        }
+
 
         // Git merge
 
